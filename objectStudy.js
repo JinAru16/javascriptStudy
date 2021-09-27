e2a83f93d25ec69b6406d52107ecf87d7140c30f
정찬욱 <70217158+JinAru16@users.noreply.github.com>
2021-09-27 17:54:15 +0900

    objectStudy
    객체만드는법 실습

---
+++ objectStudy.js
@@ -0,0 +1,14 @@
+//객체
+
+let identity = {
+    name : '찬욱',
+    bornYear : 1995,
+    isVeryNice : true,
+    bestLanguage : 'javascript',
+
+
+}
+
+console.log(identity.bornYear);
+console.log(identity["name"]);
+console.log(identity.name);
