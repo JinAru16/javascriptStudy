7cd445aa49d9ab5c6aa4df4dbf907e217abcc7eb
정찬욱 <70217158+JinAru16@users.noreply.github.com>
2021-10-02 13:47:04 +0900

    배열에 대해서 공부하기

---
+++ array.js
@@ -0,0 +1,11 @@
+// ㅍㅏ이썬에서 리스트랑 같음.
+// let 배열명 = []
+// 배열도 객체임.
+let AppleComputerRanking = [
+    'MacPro',
+    'iMac27',
+    'MacBookPro16',
+    'MackBookAir',
+    ]
+
+console.log(AppleComputerRanking[3]);
