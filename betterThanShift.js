c23423d34d0cd82cce3518e37683e509c3180066
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2021-10-03 14:19:41 +0900

    shift, unshift, pop, push

---
+++ betterThanShift.js
@@ -0,0 +1,19 @@
+// 배열의 메소드 (Array's Method)
+let members = ['아론렘지', '헤리케인', '무사시소코', '스콜스', '드록바', '포그바',];
+console.log(members);
+
+// 배열의 첫 요소를 삭제: shift()
+members.shift();
+console.log(members);
+
+// 배열의 마지막 요소를 삭제:pop()
+members.pop();
+console.log(members);
+
+//배열의 첫 요소로 값 추가 : unshift(value);
+members.unshift('박지성');
+console.log(members)
+
+//배열의 마지막 요소로 값 추가 : push(value);
+members.push('즐라탄');
+console.log(members);
